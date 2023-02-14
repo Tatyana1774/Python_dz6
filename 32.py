2c6036cd48af2c2b9661c7795642f9fc78bb63b9
Tatyana <tatyana.volkova1984@mail.ru>
2023-02-15 01:01:38 +0500

    Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону (т.е. не меньше заданного минимума и не больше заданного максимума)

--- 32.py
+++ 32.py
@@ -1,8 +1,8 @@
 # Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону (т.е. не меньше заданного минимума и не больше заданного максимума)
 from random import randint
  
-min = 3
-max = 8
+min = 2
+max = 15
 data = [randint(1, 10) for _ in range(20)]
 print("Массив: ", data, sep='\n')
 indexes = [i for i, j in enumerate(data) if min <= j <= max]
